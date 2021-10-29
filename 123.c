ded8cc6a3ec441c3b3dcfd321503944e90ec0d7c
DAVARASINGH YASWANTH SAI KIRAN <92934648+Yaswanth-19@users.noreply.github.com>
2021-10-29 00:00:51 -0700

    Add files via upload

---
+++ 123.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	printf("hello world");
+	printf("\tyaswanth sai kiran");
+	printf("\nyaswant sai kiran");
+	printf("\ny\na\ns\nw\na\nn\nt\nh");
+	printf("\none person writtrn by\"one book\"");
+}
