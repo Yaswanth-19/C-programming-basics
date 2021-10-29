7d54ef7e9a5d5c5a7c7192b085deaf4ec19295dc
DAVARASINGH YASWANTH SAI KIRAN <92934648+Yaswanth-19@users.noreply.github.com>
2021-10-29 00:00:24 -0700

    Add files via upload

---
+++ Data types.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age = 16;
+	cgpa = 8.5;
+	initial = 'D';
+	
+	//OUTPUT
+	printf("Age of the student is %d\n" , age);
+	printf("cgpa of the student is %f\n" , cgpa);
+	printf("student surname starts with %c\n" , initial);
+}
