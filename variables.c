09425b83d8c34d0e05ecf806a15304bc0a7438c2
DAVARASINGH YASWANTH SAI KIRAN <92934648+Yaswanth-19@users.noreply.github.com>
2021-10-29 23:44:04 -0700

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	//Variables
+	int num1=10,num2=20,num3=30,num4=40,sum=num1+num2+num3+num4;
+	//output
+	printf("sum of %d and %d and %d and %d is %d",num1, num2, num3, num4, sum);
+}
