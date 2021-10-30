39ab697150fd8d76a4089bacd421440ef841ceea
DAVARASINGH YASWANTH SAI KIRAN <92934648+Yaswanth-19@users.noreply.github.com>
2021-10-29 23:12:50 -0700

    Add files via upload

---
+++ yashh.1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//Variabls
+	int num1, num2, sum;
+	
+	//Initialization
+	num1 = 123;
+	num2 = 456;
+	
+	//Calculatin
+	sum = num1 + num2;
+	
+	//output
+	printf("%d" , sum);
+}
